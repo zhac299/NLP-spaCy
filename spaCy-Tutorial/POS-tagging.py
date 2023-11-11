30d6d43db5be2111bcfa3c6f2f10a3617d7c2916
zhac299 <zhac299@live.rhul.ac.uk>
2023-11-11 21:26:38 +0000

    Part-of-Speech Tagging

---
+++ spaCy-Tutorial/POS-tagging.py
@@ -0,0 +1,13 @@
+import spacy 
+nlp = spacy.load('en_core_web_sm')
+
+# Create an nlp object
+doc = nlp("He went to play basketball")
+ 
+# Iterate over the tokens
+for token in doc:
+    # Print the token and its part-of-speech tag
+    print(token.text, "-->", token.pos_)
+
+# spacy.explain("PART") explains the part
+print(spacy.explain("PART"))
