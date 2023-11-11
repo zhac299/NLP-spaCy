b0e8299654657a2371c03674f4717ac55ddfd77f
zhac299 <zhac299@live.rhul.ac.uk>
2023-11-11 21:26:57 +0000

    output pipeline components

--- spaCy-Tutorial/nlp.py
+++ spaCy-Tutorial/nlp.py
@@ -1,5 +1,12 @@
 import spacy
+
 nlp = spacy.load('en_core_web_sm')
 
 # Create an nlp object
 doc = nlp("He went to play basketball")
+
+# Returns active pipeline componenets
+print(nlp.pipe_names)
+
+# Disables selected pipeline componenets
+# nlp.disable_pipes('tagger', 'parser')
